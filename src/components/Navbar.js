1fd48bf87c653d86cbde55f4baf99fce6c063124
Bernadetta Fryczkowska <bfryczkowska@gmail.com>
2021-05-12 22:37:52 +0200

    feat: add mobile styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,29 @@
-const Navbar = () => (
-  <div className="navbar">
-    <img alt="logo" src="/logo.png" className="navbar__logo" />
-    <nav className="navbar__wrapper">
-      <ul className="navbar__list">
-        <li className="navbar__list-item">Features</li>
-        <li className="navbar__list-item">Apps</li>
-        <li className="navbar__list-item">Pricing</li>
-        <li className="navbar__list-item">Knowledge Base</li>
-        <li className="navbar__list-item">Businness</li>
-      </ul>
-      <button className="navbar__button">Log in</button>
-      <button className="navbar__button -purple">Sign up</button>
-    </nav>
-  </div>
-);
+import { useState } from "react";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <img alt="logo" src="/logo.png" className="navbar__logo" />
+      <button className="navbar__burger" onClick={() => setShowMenu(!showMenu)}>
+        <div className="navbar__burger-bar" />
+        <div className="navbar__burger-bar" />
+        <div className="navbar__burger-bar" />
+      </button>
+      <nav className={`navbar__wrapper ${showMenu && '-displayed'}`}>
+        <ul className="navbar__list">
+          <li className="navbar__list-item">Features</li>
+          <li className="navbar__list-item">Apps</li>
+          <li className="navbar__list-item">Pricing</li>
+          <li className="navbar__list-item">Knowledge Base</li>
+          <li className="navbar__list-item">Businness</li>
+        </ul>
+        <button className="navbar__button">Log in</button>
+        <button className="navbar__button -purple">Sign up</button>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
